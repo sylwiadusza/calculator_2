452454a6a345b4b0a0c8056d3c2b05ef23ece428
sylwiadusza <sylwiadusza280@gmail.com>
2022-12-01 21:30:39 +0100

    metoda_run

---
+++ src/main/Calculator.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Calculator {
+    public double sum(double x, double y){
+        return x + y;
+    }
+    public double subtraction(double x, double y){
+        return x - y;
+    }
+    public double multiplication(double x, double y){
+        return x *y;
+    }
+    public double division (double x, double y){
+        return x / y;
+    }
+}
